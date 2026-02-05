58fdd595694adc4adf284e8ff4fc6717bbdc920f
CottonMouth <itsadarsh49@gmail.com>
2026-02-06 01:36:02 +0530

    Update Mod.cpp

--- Mod.cpp
+++ Mod.cpp
@@ -4,3 +4,9 @@ x = ((x % m) + m) % m;
 
 // traversal in circular array 
 final_index = ((start + jump) % n + n) % n;
+
+
+// Safe modulo for anything that can go negative
+int modNorm(long long x, int m) {
+    return ((x % m) + m) % m;
+}
