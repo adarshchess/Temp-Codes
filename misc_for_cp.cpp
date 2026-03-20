6bc06a783f883252cbe1aab41cce5defa6ec9e1c
CottonMouth <itsadarsh49@gmail.com>
2026-03-20 23:43:32 +0530

    Implement sliding window technique in misc_for_cp.cpp
    Added sliding window two pointers implementation.

--- misc_for_cp.cpp
+++ misc_for_cp.cpp
@@ -8,3 +8,16 @@ pref[0] = a[0];
 for(int i = 1; i < n; i++){
     pref[i] = pref[i-1] + a[i];
 }
+
+//  sliding window two pointers 
+
+int l = 0;
+for(int r = 0; r < n; r++){
+    // expand window
+    
+    while(condition breaks){
+        l++; // shrink
+    }
+    
+    // process valid window
+}
