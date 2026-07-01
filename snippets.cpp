c604bdddd5ce54f93d28fc689438d5fcd23ac50b
CottonMouth <itsadarsh49@gmail.com>
2026-07-01 17:29:46 +0530

    Update snippets.cpp

--- snippets.cpp
+++ snippets.cpp
@@ -1,4 +1,16 @@
+//palindrome check 
+bool isPalindrome(const string &s) {
+    int l = 0, r = s.size() - 1;
 
+    while (l < r) {
+        if (s[l] != s[r])
+            return false;
+        l++;
+        r--;
+    }
+
+    return true;
+}
 
 // find missing no from o to n using xor 
 int ans = 0;
